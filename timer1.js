a9ee9dc7b693e14d67e9d468cc35439e83ce1e73
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-01 15:27:05 -0500

    Can't hear beep?, Added visual text instead

--- timer1.js
+++ timer1.js
@@ -3,11 +3,19 @@
 
 // Add command line arguments (User determines the number of alarms);
 const myArgs = process.argv.slice(2); // Will be an array... How do we iterate? => for c style or of loop?
+const result = [];
 
 
 for (let arg of myArgs) { // USER determined... Therefore no limit
-  console.log('myArgs: ', arg);
-
-
+  if (arg <= 0) {
+    return console.log(`Please numbers greater than 0! ${result}`);
+  }
+  if (isNaN(arg) /*|| arg === undefined*/) {  // not working?
+    return console.log(`Please enter a number! ${result}`);
+  }
+  // console.log('myArgs: ', arg); // Just displays arg values
+  process.stdout.write(`TIMER WILL BEEP IN: ${arg} seconds \n`);
+  setTimeout(() => {
+    process.stdout.write('\x07' + `${arg} *BEEP \n`);
+  }, arg * 1000);
 }
-process.stdout.write('\x07');
